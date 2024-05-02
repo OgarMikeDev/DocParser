6263e4a540ba44112702c2f96c05b4990b3cc3cf
Mike <ogar.m.v@mail.ru>
2024-05-02 10:51:43 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,29 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String htmlParse = parseSite("data/code.html");
+        Document document = Jsoup.parse(htmlParse);
+        Elements elements = document.select("a.link");
+
+        elements.forEach(System.out::println);
+        //System.out.println(htmlParse);
+    }
+
+    public static String parseSite(String path) {
+        StringBuilder builder = new StringBuilder();
+        try {
+            List<String> allLines = Files.readAllLines(Paths.get(path));
+            allLines.forEach(line -> builder.append(line).append("\n"));
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+        return builder.toString();
+    }
+}
